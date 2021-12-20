a1430a9c4c403ddf00257ac26249ba215261f09d
Lynn Detillier <lrdetillier93@gmail.com>
2021-12-20 16:09:09 -0600

    initial commit

---
+++ LoopChallenges/Factorial.js
@@ -0,0 +1,6 @@
+var product = 1
+for(i=2; i<=12; i++){
+    product = product * i
+}
+
+console.log(product)
---
+++ LoopChallenges/Sigma.js
@@ -0,0 +1,6 @@
+var sum = 1
+for( i=2; i<=100; i++){
+    sum = sum + i
+}
+
+console.log(sum)
