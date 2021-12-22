2c2fd9b3f79a39622940b9ddb377aa4720a34f5a
Lynn Detillier <lrdetillier93@gmail.com>
2021-12-21 18:10:32 -0600

    initial commit

---
+++ LikeButtons/script.js
@@ -0,0 +1,18 @@
+var likene = 1;
+function likeneil(){
+    var span = document.querySelector(".Neil");
+    span.innerText = likene++
+    return likene
+}
+var likeni = 1;
+function likenic(){
+var span = document.querySelector(".Nic");
+span.innerText = likeni++
+return likeni
+}
+var likej = 1;
+function likejim(){
+    var span = document.querySelector(".Jim");
+    span.innerText = likej++
+    return likej
+}
